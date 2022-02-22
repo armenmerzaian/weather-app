d511b4dc26e9d871b78104d585e7bffe44f263b6
Armen Merzaian <amerzanian@myseneca.ca>
2022-02-21 19:36:58 -0500

    on load current location shows weather

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,23 +5,19 @@ import useAPI from './useAPI';
 import WeatherCard from './WeatherCard';
 
 export default function Body(props) {
-    const [currentLocation] = useState(props.location);
     const [data, setData] = useState({});
+    const location = props.location;
 
-    const apiData = useAPI(currentLocation.lat, currentLocation.long);
-    console.log("after getting apiData in body component");
+    const apiData = useAPI(location.lat, location.long);
     useEffect(() => {
         setData(apiData);
-    }, []);
+    },[apiData]);
     
   return (
     <Container className="v-100">
         <SearchComponent />
         <Container className="d-flex flex-column justify-content-center" style={{marginTop: 10 + 'px', marginBottom: 10 + 'px'}}>
             <WeatherCard data={data}/>
-            <WeatherCard />
-            <WeatherCard />
-            <WeatherCard />
         </Container>
     </Container>
   )
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -19,13 +19,12 @@ export default function WeatherCard(props) {
   const [sunset, setSunset] = useState(props.data.sys.sunset);
   const [lat, setLat] = useState(props.data.coord.lat);
   const [long, setLong] = useState(props.data.coord.lon);
-
   return (
     <div>
       <Card style={{ margin: '10px', padding: '10px',backgroundColor: 'rgba(0,0,0,.03)' }}>
         <Card.Header style={{ backgroundColor: 'rgba(0,0,0,0)' }}>
           <Card.Title>
-            <span>{name},</span>
+            <span>{name}, </span>
             <span>{country}</span>
             <span>Image</span>
           </Card.Title>
@@ -42,11 +41,11 @@ export default function WeatherCard(props) {
                   <i><span id="weather-desc">{weatherDesc}</span></i>
                 </li>
                 <li className="list-group-item">
-                  <span id="main-temp" className="badge bg-primary">{temp} <span className="temp-units">&#8451</span></span>
-                  <span id="main-feels-like"> Feels like: </span>{feelsLike} <span className="temp-units">&#8451</span>
+                  <span id="main-temp" className="badge bg-primary">{temp} <span className="temp-units">°C</span></span>
+                  <span id="main-feels-like"> Feels like: </span>{feelsLike} <span className="temp-units">°C</span>
                   <br />
-                  <span id="main-temp-min"> Minimum: </span> {tempMin}<span className="temp-units">&#8451</span>
-                  <span id="main-temp-max"> Maximum: </span> {tempMax}<span className="temp-units">&#8451</span>
+                  <span id="main-temp-min"> Min: </span> {tempMin}<span className="temp-units">°C</span>
+                  <span id="main-temp-max"> Max: </span> {tempMax}<span className="temp-units">°C</span>
                 </li>
                 <li className="list-group-item">
                   <span id="main-humidity"> Humidity: {humidity}</span><span>%</span>
@@ -56,7 +55,6 @@ export default function WeatherCard(props) {
                 <li className="list-group-item">
                   <span id="sys-sunrise"> Sunrise: {moment.unix(sunrise).format('LT')}</span>
                   <span id="sys-sunset"> Sunset: {moment.unix(sunset).format('LT')}</span>
-                  <br />
                   <span id="coord-lat"> [{lat}, </span>
                   <span id="coord-long">{long}]</span>
                 </li>
