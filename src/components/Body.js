bd60a91a43d805148ac1d6ef13b0dfb4ee87285e
Armen Merzaian <amerzanian@myseneca.ca>
2022-02-22 12:37:07 -0500

    lots of bugs

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,24 +1,29 @@
-import React, {useEffect, useState} from 'react';
-import {Container} from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Container, Alert } from 'react-bootstrap';
 import SearchComponent from './SearchComponent';
 import useAPI from './useAPI';
 import WeatherCard from './WeatherCard';
 
-export default function Body(props) {
-    const [data, setData] = useState({});
-    const location = props.location;
+function NoLocation() {
+    return <Alert variant="primary">Location not found.</Alert>
+}
 
+export default function Body(props) {
+    const location = props.location;
     const apiData = useAPI(location.lat, location.long);
-    useEffect(() => {
-        setData(apiData);
-    },[apiData]);
+    const [search, setSearch] = useState('');
     
-  return (
-    <Container className="v-100">
-        <SearchComponent />
-        <Container className="d-flex flex-column justify-content-center" style={{marginTop: 10 + 'px', marginBottom: 10 + 'px'}}>
-            <WeatherCard data={data}/>
+
+    function renderCard(data) {
+        return (<WeatherCard data={data} />);
+    };
+
+    return (
+        <Container className="v-100">
+            <SearchComponent setSearch={setSearch} />
+            <Container className="d-flex flex-column justify-content-center" style={{ marginTop: 10 + 'px', marginBottom: 10 + 'px' }}>
+                {apiData ? renderCard(apiData) : <NoLocation /> }
+            </Container>
         </Container>
-    </Container>
-  )
+    )
 }
--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Form, FormGroup, Button} from 'react-bootstrap';
+import {Route} from 'react-router';
 
-export default function SearchComponent() {
-  
+export default function SearchComponent({setSearch}) {
+    const [searchString, setSearchString] = useState('');
+
+    function handleChange(e){
+        setSearchString(e.target.value);
+    }
+    function handleSubmit(e){
+        e.preventDefault();
+        setSearch(searchString);
+    }
+
     return (
-    <Form className="container">
+    <Form className="container" onSubmit={handleSubmit}>
         <FormGroup className="d-flex justify-content-between">
-            <Form.Control className="me-2"type="text" placeholder="Enter City..."/>
+            <Form.Control className="me-2"type="text" placeholder="Enter City..." value={searchString} onChange={handleChange}/>
             <Button variant="primary" type="submit">Search</Button>
         </FormGroup>
     </Form>
